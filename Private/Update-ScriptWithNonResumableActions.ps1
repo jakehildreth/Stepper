e26a2079a36553f9cc530ba8ba9dbb1eee641564
Jake Hildreth <jakehildreth@gmail.com>
2025-12-19 18:49:57 -0500

    feat: update this function to keep non-resumable code in its current location when wrapped with New-Step {}

--- Private/Update-ScriptWithNonResumableActions.ps1
+++ Private/Update-ScriptWithNonResumableActions.ps1
@@ -33,33 +33,72 @@ function Update-ScriptWithNonResumableActions {
     )
 
     $newScriptLines = @()
-    $linesToWrap = @()
     $linesToMove = @()
     $lastBlock = $NewStepBlocks[$NewStepBlocks.Count - 1]
 
-    # Separate lines by action
+    # Group lines to wrap by consecutive sequences
+    $linesToWrap = @($Actions.Keys | Where-Object { $Actions[$_].Action -eq 'Wrap' } | Sort-Object)
+    $wrapGroups = @()
+    if ($linesToWrap.Count -gt 0) {
+        $currentGroup = @($linesToWrap[0])
+        for ($i = 1; $i -lt $linesToWrap.Count; $i++) {
+            if ($linesToWrap[$i] -eq $linesToWrap[$i - 1] + 1) {
+                $currentGroup += $linesToWrap[$i]
+            } else {
+                $wrapGroups += ,@($currentGroup)
+                $currentGroup = @($linesToWrap[$i])
+            }
+        }
+        $wrapGroups += ,@($currentGroup)
+    }
+
+    # Separate lines to move
     foreach ($lineIdx in $Actions.Keys) {
-        switch ($Actions[$lineIdx].Action) {
-            'Wrap' { $linesToWrap += $lineIdx }
-            'Move' { $linesToMove += $lineIdx }
+        if ($Actions[$lineIdx].Action -eq 'Move') {
+            $linesToMove += $lineIdx
         }
     }
 
-    # Copy all lines except ones being removed
+    # Process all lines
+    $wrappedLines = @{}
+    foreach ($group in $wrapGroups) {
+        foreach ($idx in $group) {
+            $wrappedLines[$idx] = $true
+        }
+    }
+
     for ($i = 0; $i -lt $ScriptLines.Count; $i++) {
-        if (-not $Actions.ContainsKey($i)) {
-            $newScriptLines += $ScriptLines[$i]
+        # Check if this line starts a wrap group
+        $startsWrapGroup = $false
+        $wrapGroup = $null
+        foreach ($group in $wrapGroups) {
+            if ($group[0] -eq $i) {
+                $startsWrapGroup = $true
+                $wrapGroup = $group
+                break
+            }
         }
 
-        # After the last New-Step block, add wrapped code
-        if ($i -eq $lastBlock.End -and $linesToWrap.Count -gt 0) {
-            $newScriptLines += ""
+        if ($startsWrapGroup) {
+            # Start the New-Step block
             $newScriptLines += "New-Step {"
-            foreach ($lineIdx in ($linesToWrap | Sort-Object)) {
-                $newScriptLines += "    $($ScriptLines[$lineIdx])"
+            foreach ($idx in $wrapGroup) {
+                $newScriptLines += "    $($ScriptLines[$idx])"
             }
             $newScriptLines += "}"
+            # Skip to after this group
+            $i = $wrapGroup[-1]
+            continue
         }
+
+        # Skip lines that are wrapped (but not the start of a group) or moved or deleted
+        if ($wrappedLines.ContainsKey($i) -or 
+            ($Actions.ContainsKey($i) -and $Actions[$i].Action -in @('Move', 'Delete'))) {
+            continue
+        }
+
+        # Copy line as-is
+        $newScriptLines += $ScriptLines[$i]
     }
 
     # Add moved code at the end
