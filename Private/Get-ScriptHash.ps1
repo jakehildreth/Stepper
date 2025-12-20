952926578b524e64c6008c39b8d7c2f97ed1793d
Jake Hildreth <93942157+jakehildreth@users.noreply.github.com>
2025-12-19 22:24:13 -0500

    test: make a function intentionally fail to ensure bad code doesn't get published to PSGallery

--- Private/Get-ScriptHash.ps1
+++ Private/Get-ScriptHash.ps1
@@ -2,14 +2,14 @@ function Get-ScriptHash {
     <#
     .SYNOPSIS
         Calculates SHA256 hash of a script file.
-    
+
     .DESCRIPTION
         Reads the content of a script file and returns its SHA256 hash.
         Used to detect if the script has been modified since the last run.
-    
+
     .PARAMETER ScriptPath
         The path to the script file.
-    
+
     .OUTPUTS
         System.String - SHA256 hash of the script content
     #>
@@ -18,7 +18,7 @@ function Get-ScriptHash {
         [Parameter(Mandatory)]
         [string]$ScriptPath
     )
-    
+    throw 'intentional failure'
     try {
         $content = Get-Content -Path $ScriptPath -Raw -ErrorAction Stop
         $bytes = [System.Text.Encoding]::UTF8.GetBytes($content)
