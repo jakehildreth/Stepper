38a152082386fbe5a084de59f06fdee9b18caf07
Jake Hildreth <93942157+TrimarcJake@users.noreply.github.com>
2025-12-19 20:29:07 -0500

    feat: update Test.ps1 to fail in multiple ways

--- Examples/Test.ps1
+++ Examples/Test.ps1
@@ -1,20 +1,17 @@
-[CmdletBinding()]
-param()
 
-Import-Module "$PSScriptRoot\..\Stepper.psd1" -Force
-
+Write-Host 'Non-resumable code before first step.'
 New-Step {
     $Stepper.AllProcesses = Get-Process
     $Stepper.ChildItems = Get-ChildItem
 }
 
-Write-Host 'Non-resumable code between steps.'
+Write-Host 'Non-resumable code between steps 1 and 2.'
 
 New-Step {
     $Stepper.AllProcesses | Select-Object -Last 3
 }
 
-Write-Host 'Non-resumable code between steps.'
+Write-Host 'Non-resumable code between steps 2 and 3.'
 
 New-Step {
     $Stepper.ChildItems| Select-Object -Last 3
