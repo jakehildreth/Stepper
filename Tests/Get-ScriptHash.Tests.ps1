375717f8a348e479bdf08841d76b85d55a279da6
Jake Hildreth <93942157+jakehildreth@users.noreply.github.com>
2025-12-19 22:02:22 -0500

    test: clean up whitespace in Pester tests for Get-ScriptHash

--- Tests/Get-ScriptHash.Tests.ps1
+++ Tests/Get-ScriptHash.Tests.ps1
@@ -21,9 +21,9 @@ Describe 'Get-ScriptHash' {
     Context 'When calculating hash for script files' {
         It 'Should return a 64-character hex string' {
             'Write-Host "Test"' | Out-File -FilePath $TestFile -Encoding UTF8
-            
+
             $hash = Get-ScriptHash -ScriptPath $TestFile
-            
+
             $hash | Should -BeOfType [string]
             $hash.Length | Should -Be 64
             $hash | Should -Match '^[0-9A-F]+$'
@@ -32,48 +32,48 @@ Describe 'Get-ScriptHash' {
         It 'Should return consistent hash for same content' {
             $content = 'Write-Host "Hello World"'
             $content | Out-File -FilePath $TestFile -Encoding UTF8
-            
+
             $hash1 = Get-ScriptHash -ScriptPath $TestFile
             $hash2 = Get-ScriptHash -ScriptPath $TestFile
-            
+
             $hash1 | Should -Be $hash2
         }
 
         It 'Should return different hash for different content' {
             'Write-Host "Test 1"' | Out-File -FilePath $TestFile -Encoding UTF8
             $hash1 = Get-ScriptHash -ScriptPath $TestFile
-            
+
             'Write-Host "Test 2"' | Out-File -FilePath $TestFile -Encoding UTF8
             $hash2 = Get-ScriptHash -ScriptPath $TestFile
-            
+
             $hash1 | Should -Not -Be $hash2
         }
 
         It 'Should detect whitespace changes' {
             'Write-Host "Test"' | Out-File -FilePath $TestFile -Encoding UTF8
             $hash1 = Get-ScriptHash -ScriptPath $TestFile
-            
+
             'Write-Host  "Test"' | Out-File -FilePath $TestFile -Encoding UTF8
             $hash2 = Get-ScriptHash -ScriptPath $TestFile
-            
+
             $hash1 | Should -Not -Be $hash2
         }
 
         It 'Should detect line ending changes' {
             "Line1`nLine2" | Out-File -FilePath $TestFile -Encoding UTF8 -NoNewline
             $hash1 = Get-ScriptHash -ScriptPath $TestFile
-            
+
             "Line1`r`nLine2" | Out-File -FilePath $TestFile -Encoding UTF8 -NoNewline
             $hash2 = Get-ScriptHash -ScriptPath $TestFile
-            
+
             $hash1 | Should -Not -Be $hash2
         }
 
         It 'Should handle empty files' {
             ' ' | Out-File -FilePath $TestFile -Encoding UTF8 -NoNewline
-            
+
             $hash = Get-ScriptHash -ScriptPath $TestFile
-            
+
             $hash | Should -Not -BeNullOrEmpty
             $hash.Length | Should -Be 64
         }
@@ -88,9 +88,9 @@ function Test-Function {
 Test-Function -Name "World"
 '@
             $script | Out-File -FilePath $TestFile -Encoding UTF8
-            
+
             $hash = Get-ScriptHash -ScriptPath $TestFile
-            
+
             $hash | Should -Not -BeNullOrEmpty
             $hash.Length | Should -Be 64
         }
@@ -99,7 +99,7 @@ Test-Function -Name "World"
     Context 'Error handling' {
         It 'Should throw when file does not exist' {
             $nonExistentFile = Join-Path $TestDrive "does-not-exist.ps1"
-            
+
             { Get-ScriptHash -ScriptPath $nonExistentFile } | Should -Throw
         }
 
