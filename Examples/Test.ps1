8721fff01b3b1d3c32a7ef69c046f7b531a8f06a
Jake Hildreth <93942157+jakehildreth@users.noreply.github.com>
2026-03-07 13:12:55 -0500

    fix: revert test script to original version.

--- Examples/Test.ps1
+++ Examples/Test.ps1
@@ -1,38 +1,28 @@
-#requires -Modules Stepper
-[CmdletBinding()]
-param()
+Write-Output 'This code is useless. Delete it!' | Out-Null
 
-
-New-Step 'Enter Your Name' {
+New-Step {
     $Stepper.Name = Read-Host 'Enter your name'
-    Write-Verbose "Running '$($Stepper.StepName)' (step $($Stepper.StepNumber))"
 }
 
-#region Stepper ignore
 Write-Output 'This code should execute every time the script runs.' | Out-Null
 Write-Output 'Mark this block w/ a special comment so Stepper ignores it.' |
     Out-Null
 $Stepper.ProcessCount = (Get-Process).Count
 $Stepper.ItemCount = (Get-ChildItem).Count
 $Stepper.CollectionTime = Get-Date
-#endregion Stepper ignore
 
-New-Step 'Display Results' {
+New-Step {
     $response = Read-Host 'Do you want to simulate a crash? [Y/n]'
     if ($response -eq '' -or $response -eq 'Y' -or $response -eq 'y') {
         Write-Host ""
         Write-Host "Oh no! A crash..." -ForegroundColor Red
         exit
     }
-    Write-Host "Hey, $($Stepper.Name)! (running '$($Stepper.StepName)')"
+    Write-Host "Hey, $($Stepper.Name)!"
     Write-Host "There are $($Stepper.ProcessCount) processes currently running."
 }
 
-New-Step {
-    Write-Output "Make this code resumable. Wrap it in New-Step {}." | Out-Null
-    Write-Host "There are $($Stepper.ItemCount) items in this directory."
-}
+Write-Output "Make this code resumable. Wrap it in New-Step {}." | Out-Null
+Write-Host "There are $($Stepper.ItemCount) items in this directory."
 
 #
-
-Stop-Stepper
